6f7d3f7d66138af158bbd36912f54c96d14b2a05
CharbelBijani <charbel.bijani@gmail.com>
2022-06-22 17:11:11 +0200

    22/06/2022 - 17:11

--- app/src/main/java/com/dam/reptel/MainActivity.java
+++ app/src/main/java/com/dam/reptel/MainActivity.java
@@ -2,13 +2,53 @@ package com.dam.reptel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Variables globales
+     * declaration des deux boutons
+     * **/
+
+    Button btnSenregistrer, btnSeConnecter;
+
+    /** Initialisation et lien entre java et le design **/
+
+    private void initUI(){
+        btnSenregistrer = findViewById(R.id.btnSenregistrer);
+        btnSeConnecter = findViewById(R.id.btnSeConnecter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUI();
+
+        btnSenregistrer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                String titre=btnSenregistrer.getText().toString();
+                intent.putExtra("TitrePage", titre);
+                startActivity(intent);
+            }
+        });
+
+        btnSeConnecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                String titre=btnSeConnecter.getText().toString();
+                intent.putExtra("TitrePage", titre);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/dam/reptel/SignupActivity.java
+++ app/src/main/java/com/dam/reptel/SignupActivity.java
@@ -2,13 +2,80 @@ package com.dam.reptel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
 
 public class SignupActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignupActivity";
+
+    /** variables globales **/
+
+    private TextInputEditText etPrenomNom, etNumTel, etMotPass, etConfMotPass;
+    private String nom, numTel, motPasse, confMotPass;
+    private Button btnSenregistrer;
+
+    /** ajout de FirebaseAuth pour enregistrer l'utilisateur **/
+    private FirebaseAuth firebaseAuth;
+
+    /** initialisation **/
+    private void initUI(){
+        etPrenomNom = findViewById(R.id.etPrenomNom);
+        etNumTel = findViewById(R.id.etNumTel);
+        etMotPass = findViewById(R.id.etMotPass);
+        etConfMotPass = findViewById(R.id.etConfMotPass);
+        btnSenregistrer = findViewById(R.id.btnSenregistrer);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        /** recuperation de la valeur de l'intent **/
+        Intent intent = getIntent();
+        String titre = intent.getStringExtra("TitrePage");
+        setTitle(titre);
+
+        initUI();
+    }
+
+    public void btnSignupClick(View view) {
+        nom=etPrenomNom.getText().toString().trim();
+        numTel=etNumTel.getText().toString().trim();
+        motPasse=etMotPass.getText().toString().trim();
+        confMotPass=etConfMotPass.getText().toString().trim();
+
+        //verifications si les cases ne sont pas vides
+        if (nom.equals("")){
+            etPrenomNom.setError("Entrez vos prénom et nom");
+        } else if (numTel.equals("")){
+            etNumTel.setError("Entrez votre numéro de téléphone");
+        } else if (numTel.length()<10){
+            etNumTel.setError("Entrez un numero de tel correct");
+        } else if (motPasse.equals("")){
+            etMotPass.setError("Entrez un mot de passe");
+        } else if (confMotPass.equals("")){
+            etConfMotPass.setError("Confirmez le mot de passe");
+        } else if (!motPasse.equals(confMotPass)){
+            etConfMotPass.setError("Mots de passe ne correspondent pas");
+        }
+        // Si tout est bon on s'enregistre.
+        else {
+            // on verifie la connection a internet
+            if (Util.connectionAvailable(this))
+        }
+
+
     }
 }
