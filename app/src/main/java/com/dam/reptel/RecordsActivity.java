4366428382b732a82524757e7052fa9781375fd9
PHO <hoangp1520@gmail.com>
2022-07-01 10:06:45 +0200

    v01.1 Panneau des enregistrements

---
+++ app/src/main/java/com/dam/reptel/RecordsActivity.java
@@ -0,0 +1,14 @@
+package com.dam.reptel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_records);
+    }
+}
