e532ec17ba154c62dfb89cd3d98801a0231b60af
PHO <hoangp1520@gmail.com>
2022-07-05 10:06:38 +0200

    v01.2 Panneau des enregistrements (activity_records.xml) avec CardView (item_records.xml), modèle (ModelRecords.java) et l'adapter des enregistrements (AdapterRecords.java)

--- app/src/main/java/com/dam/reptel/AdapterContacts.java
+++ app/src/main/java/com/dam/reptel/AdapterContacts.java
@@ -60,7 +60,8 @@ public class AdapterContacts extends FirestoreRecyclerAdapter<ModelRecord, Adapt
         contactsViewHolder.tv_timestamp.setText(newDate);
 
         // recherche de la photo du contact dans les contacts a partir du numero de tel
-        // photo = getDisplayPhoto(contactsViewHolder.tv_nomcontact.getContext(), tel);
+        photo = getDisplayPhoto(contactsViewHolder.tv_nomcontact.getContext(), tel);
+        image.setImageBitmap(photo);
 
         // ajout des options pour afficher les photos des contacts.
         RequestOptions options = new RequestOptions()
